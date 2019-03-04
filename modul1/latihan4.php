23cb2be190ae411e9e08fb74e82ca898b95527fa
dnrftrvania <dinar_fitri_27rpl@student.smktelkom-mlg.sch.id>
2019-03-04 18:31:51 +0700

    first commit

---
+++ modul1/latihan4.php
@@ -0,0 +1,8 @@
+<?php
+    $nama = "Vania";
+    function tampil_nama(){
+        global$nama;
+        echo "Nama Saya : ".$nama;
+    }
+    tampil_nama();
+?>
---
+++ modul3/Latihan3.php
@@ -0,0 +1,9 @@
+<?php
+    $user = "vania";
+    $pass = "123";
+    if ($user == "vania" && $pass == "123") {
+        echo "Login Berhasil";
+    } else {
+        echo "Login Gagal";
+    }
+?>
